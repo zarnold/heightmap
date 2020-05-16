587e85922fb618029ab7b4c820851b5169e101d5
Arnold ZEPHIR <arnold.zephir@prevision.io>
2020-05-16 18:52:12 +0200

    feat: Blur and sharpen image for better rendering

--- draw_contour.py
+++ draw_contour.py
@@ -1,7 +1,9 @@
+# https://terrain.party/
+
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 from matplotlib import cm
-from PIL import Image
+from PIL import Image,ImageFilter
 import numpy as np
 import os
 
@@ -9,54 +11,77 @@ import os
 PATH_TO_HEIGTHMAP = os.path.join("heightmap")
 
 
-def fileToHeightmap(image_name):
-    """Generate a 2D numpy array of pixel intensity (heightmap) from a file
-
-    Arguments:
-        image_name {String} -- name of the image
-
-    Returns:
-        np.array -- A 2D numpy array
-    """
-    path = os.path.join(PATH_TO_HEIGTHMAP, image_name)
-    # to greyscale
-    imageGrayAlpha = Image.open(path).convert('LA')
-    imageNumpyArray = np.array(imageGrayAlpha)
-    # do not forget the type otherwise its an uint8
-    heigthMap = imageNumpyArray[:, :, 0].astype(int)
-    return heigthMap
 
 
+def visualisation(img):
+    plt.clf()
+    x = np.arange(len(img[0]))[::-1]
+    y = np.arange(len(img))
+    X, Y = np.meshgrid(x, y)
+    fig = plt.figure()
+    # 2D Flat image
+    ax = fig.add_subplot(221)
+    ax.imshow(img, cmap=plt.get_cmap('terrain'), vmin=0, vmax=255)
+    # 3D mapp
+    ax = fig.add_subplot(222, projection='3d')
+    ax.plot_surface(X, Y, img, cmap=cm.terrain,
+                    linewidth=0, antialiased=False, shade=True)
+    ax.set_zlim3d(0, 1000)
+    # Wireframe
+    ax = fig.add_subplot(223, projection='3d')
+    ax.plot_wireframe(X, Y, img, rstride=50, cstride=50)
+    ax.set_zlim3d(0, 380)
+    # contour map
+    ax = fig.add_subplot(224, projection='3d')
+    ax.contour(X, Y, img, colors="k", linewidths=0.02,
+               antialiased=False, shade=True)
+    ax.set_zlim3d(0, 1000)
+    plt.show()
 
-img = fileToHeightmap("france.png")
 
-x = np.arange(len(img[0]))[::-1]
-y = np.arange(len(img))
+
+
+
+from matplotlib.pyplot import figure
+figure(num=None, figsize=(11, 11), dpi=120, facecolor='w', edgecolor='k')
+
+
+file="paris_large"
+path = os.path.join(PATH_TO_HEIGTHMAP,"{}.png".format(file))
+
+# Pillow does not support 16 bit png
+img = Image.open(path)
+img_float = Image.fromarray(np.divide(np.array(img), 2**8-1))
+original_image = img_float.convert('L')
+
+
+B=20
+blurred_image = original_image.filter(ImageFilter.GaussianBlur(B)).filter(ImageFilter.SHARPEN)
+
+
+
+
+img = np.array(blurred_image)
+visualisation(img)
+x = np.arange(len(img[0]))
+y = np.arange(len(img))[::-1]
 X, Y = np.meshgrid(x, y)
+N=15
+levels = np.linspace(img.min(),img.max(), N)
+cmap = plt.cm.get_cmap('jet')    
+
+plt.clf()
+fig= plt.figure(num=None, figsize=(11, 11), dpi=80, facecolor='w', edgecolor='k')
+plt.contour(X, Y, img,levels)
+plt.legend()
+plt.savefig("{}_{}_{}.png".format(file,N, B), bbox_inches='tight')
 
 
-fig = plt.figure()
+plt.clf()
+for h in levels :
+    fig= plt.figure(num=None, figsize=(11, 11), dpi=120, facecolor='w', edgecolor='k')
+    plt.contour(X, Y, img,[h],linewidths=0.6, colors="k", origin="lower")
+    plt.axis("off")
+    plt.savefig("{}_{:04.2f}.png".format(file,h), bbox_inches='tight')
 
 
-# 2D Flat image
-ax = fig.add_subplot(221)
-ax.imshow(img, cmap=plt.get_cmap('terrain'), vmin=0, vmax=255)
-
-# 3D mapp
-ax = fig.add_subplot(222, projection='3d')
-ax.plot_surface(X, Y, img, cmap=cm.terrain,
-                linewidth=0, antialiased=False, shade=True)
-ax.set_zlim3d(0, 1000)
-
-# Wireframe
-ax = fig.add_subplot(223, projection='3d')
-ax.plot_wireframe(X, Y, img, rstride=50, cstride=50)
-ax.set_zlim3d(0, 380)
-
-# contour map
-ax = fig.add_subplot(224, projection='3d')
-ax.contour(X, Y, img, cmap=cm.terrain, linewidth=0,
-           antialiased=False, shade=True)
-ax.set_zlim3d(0, 1000)
-
-plt.show()
